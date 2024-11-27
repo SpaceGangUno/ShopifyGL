edbe06aad3685d6e11b08011e829bb35d45fb239
SpaceGangUno <144416246+SpaceGangUno@users.noreply.github.com>
2024-11-26 22:35:21 -0500

    Update updateBulkInventory.js

--- inventory-automation/updateBulkInventory.js
+++ inventory-automation/updateBulkInventory.js
@@ -14,28 +14,60 @@ const shopify = axios.create({
   }
 });
 
-// Product data with SKU and quantity (batch 8)
+// Product data with SKU and quantity (batch 2 of 50)
 const inventory = [
-  { sku: '820262A', size: 'L', quantity: 3, title: 'Ailip - Grey And Black Checker Printed Shorts' },
-  { sku: 'G454993', size: 'XL', quantity: 3, title: 'Ailip - Grey And Black Checker Printed Shorts' },
-  { sku: 'L515696', size: 'XXL', quantity: 3, title: 'Ailip - Grey And Black Checker Printed Shorts' },
-  { sku: '891203A', size: 'M', quantity: 3, title: 'All Eyes on me-Jacket PL2711- Black' },
-  { sku: '3459912', size: 'M', quantity: 3, title: 'All Over Hoodie (FW25-211 Black)' },
-  { sku: 'Z692742', size: 'M', quantity: 3, title: 'At First Sight Fleece Hoodie - Black (142-321)' },
-  { sku: '708512A', size: 'Regular', quantity: 3, title: 'Blue , Grey , White Shiesty face mask (1)' },
-  { sku: '6381277', size: 'L', quantity: 3, title: 'Boomer Belts JX-6594 Gold Belt' },
-  { sku: 'G038316', size: 'M', quantity: 3, title: 'Build Ideas Fleece Hoodie (142-332) Olive' },
-  { sku: 'F907216', size: 'M-32', quantity: 3, title: 'Color Block Stacked Jeans (AJ40SK-3) - Blue' },
-  { sku: 'H222996', size: 'L-34', quantity: 3, title: 'Color Block Stacked Jeans (AJ40SK-3) - Blue' },
-  { sku: '8573232', size: 'XL', quantity: 3, title: "D'OH! Bunny Tee - Natural Tone" },
-  { sku: '3927', size: '38', quantity: 3, title: 'EV-22278 WILD TIGER BLACK DENIM SHORTS' },
-  { sku: '3933', size: '32', quantity: 3, title: 'EV-22278 WILD TIGER DENIM MED TINT SHORTS' },
-  { sku: '3940', size: 'S', quantity: 3, title: 'EV-22461E CARGO BLACK KNIT SHORTS' },
-  { sku: 'K471397', size: 'L-34', quantity: 3, title: 'Elite Corduroy Shorts - Beige' },
-  { sku: 'Q227379', size: 'Regular', quantity: 3, title: 'Exalte Face Ski Mask - Black/White (EFSM)' },
-  { sku: '771101G', size: 'Regular', quantity: 3, title: 'F - it Trucker Hat - Olive' },
-  { sku: '9203772', size: 'L', quantity: 3, title: 'FRJ2060 Coexist Multi Patches Shacket' },
-  { sku: '393535F', size: 'XL', quantity: 3, title: 'FW-11212A- Multi T-shirts' }
+  { sku: '422715P', size: 'XL-36', quantity: 3, title: 'Nylon Shorts (AR1006)- Navy' },
+  { sku: '400592S', size: 'M-32', quantity: 3, title: 'Nylon Shorts (AR1006)- Red' },
+  { sku: '6808989', size: 'XL-36', quantity: 3, title: 'Nylon Shorts (AR1006)- Red' },
+  { sku: 'E502726', size: 'M-32', quantity: 3, title: 'Nylon Shorts (AR1006)- Royal' },
+  { sku: 'C566327', size: 'XL-36', quantity: 3, title: 'Nylon Shorts (AR1006)- Royal' },
+  { sku: '3153632', size: 'L', quantity: 3, title: 'Orange And Gold Button Down' },
+  { sku: '7477', size: 'L', quantity: 3, title: 'QS7 - No Bad Vibes - Aqua' },
+  { sku: '0434', size: 'M', quantity: 3, title: 'ROOKIE BLACK JOGGERS' },
+  { sku: '9423', size: 'Regular', quantity: 3, title: 'RTH-F-19 ANIME TRUCKER HAT NAVY' },
+  { sku: '4462689', size: 'S', quantity: 3, title: 'RXNF22-WS001 Naruto Mesh Shorts' },
+  { sku: '684590Y', size: 'L', quantity: 3, title: 'RXNF22-WT001 Naruto Sasuke Shirt' },
+  { sku: '447372V', size: '32', quantity: 3, title: 'S2202 Grey Denim Shorts' },
+  { sku: '767436X', size: '36', quantity: 3, title: 'S3016T- Gray Distressed Stacked Jeans' },
+  { sku: '8215', size: 'XL', quantity: 3, title: 'SF1028 - Burgundy Never Stop' },
+  { sku: 'T939519', size: 'L-34', quantity: 3, title: 'Skinny Fit Jeans Olive - KNB3119' },
+  { sku: 'A000391', size: 'M-32', quantity: 3, title: 'Skinny Fit Ripped Jeans Black - KNB3119' },
+  { sku: 'M902697', size: 'M-32', quantity: 3, title: 'Skinny Flare Fit (KND4773) - Blue' },
+  { sku: 'E543853', size: 'L-34', quantity: 3, title: 'Skinny Jeans White - KD656' },
+  { sku: 'V956439', size: 'S-30', quantity: 3, title: 'Slim Fit (KND4768) - Blue' },
+  { sku: '5026769', size: 'S-30', quantity: 3, title: 'Slim Flare Fit (KND4772) - Blue' },
+  { sku: 'T321562', size: '36', quantity: 3, title: 'Stacked Flare Fit Jeans - Med Indigo  (642-637)' },
+  { sku: '941139G', size: 'XL-36', quantity: 3, title: 'Stitch Athletic Fit Jeans - Jet Black' },
+  { sku: '2069841', size: 'M-32', quantity: 3, title: 'Stylo Stacked Fitted Jeans - Burgundy And Black' },
+  { sku: '344918S', size: 'L-34', quantity: 3, title: 'Stylo Stacked Fitted Jeans - Burgundy And Black' },
+  { sku: 'F662162', size: 'L', quantity: 3, title: 'T474 Roar Black T-shirt' },
+  { sku: '338279A', size: 'S', quantity: 3, title: 'T474 Roar Teal T-shirt' },
+  { sku: '9471233', size: 'Regular', quantity: 3, title: 'TN5331B - Rose Money Trucker Hat - Black' },
+  { sku: 'E487755', size: 'L-34', quantity: 3, title: 'Tapestry Cargo Pants -- Khaki (642-651)' },
+  { sku: '4913070', size: 'XL', quantity: 3, title: 'Tokyo Drift Mint Tee' },
+  { sku: 'L480698', size: 'O/S', quantity: 3, title: 'Travis Astro Hat TN5313A CAMO' },
+  { sku: '5991097', size: 'L', quantity: 3, title: 'True Feelings Fleece Hoodie (142-333) Beige' },
+  { sku: '5519959', size: '32', quantity: 3, title: 'UB107 - Orange Denim Shorts' },
+  { sku: 'X268628', size: 'L', quantity: 3, title: 'US3115- Royal Blue T Shirt' },
+  { sku: '126931S', size: 'L', quantity: 3, title: 'Vessel of Grace Tee - Black' },
+  { sku: '144712D', size: 'XL', quantity: 3, title: 'Vessel of Grace Tee - Black' },
+  { sku: 'P392195', size: 'L-34', quantity: 3, title: 'Victorious Black Baggy Fit Shorts DS2099 (1)' },
+  { sku: '103817F', size: 'M-32', quantity: 3, title: 'Victorious DL1501 Indigo Baggy Fit Carpenter Jeans' },
+  { sku: '438842S', size: 'XXS-28', quantity: 3, title: 'Victorious DS2099 Lt. Indigo Baggy Fit Shorts' },
+  { sku: 'S479720', size: 'M', quantity: 3, title: 'W Black and White Shorts' },
+  { sku: '9263', size: 'L', quantity: 3, title: 'WOLF TRIBE - BLACK' },
+  { sku: '9264', size: 'XL', quantity: 3, title: 'WOLF TRIBE - BLACK' },
+  { sku: '9267', size: 'M', quantity: 3, title: 'WOLF TRIBE - BLUE' },
+  { sku: 'N869206', size: 'L', quantity: 3, title: 'WV1-004 Gold Digging S/S Shirt' },
+  { sku: '144921L', size: 'M-32', quantity: 3, title: 'Waimea Rust Stacked Jeans -M5826T' },
+  { sku: 'Y768672', size: 'M', quantity: 3, title: 'Wake Up And Be Fierce Skull Spirit Washed Cropped Tee' },
+  { sku: 'D715005', size: '30', quantity: 3, title: 'White Ripped Skinny Jeans (KNB3341)' },
+  { sku: '9562091', size: 'M', quantity: 3, title: 'Win Again Hoodie - Blue' },
+  { sku: 'M485345', size: 'S', quantity: 3, title: 'Wrathboy Wolf Head Gothic Tee (WB04-101 DK Grey)' },
+  { sku: '878748R', size: 'S', quantity: 3, title: "Y'all Invtd Hoodie (KF-6920 Vintage Black)" },
+  { sku: 'D314927', size: 'XL-36', quantity: 3, title: 'off the wok - sweat pants' },
+  { sku: '3672978', size: 'XL', quantity: 3, title: 'off the wok tees - blue' },
+  { sku: '916293B', size: 'L', quantity: 3, title: 'off the wok tees - red' }
 ];
 
 async function getLocationId() {
@@ -95,7 +127,7 @@ async function delay(ms) {
 
 async function updateInventoryLevels() {
   try {
-    console.log('=== Updating Inventory Levels (Batch 8) ===\n');
+    console.log('=== Updating Inventory Levels (Batch 2 of 50) ===\n');
     
     // Get location ID
     const locationId = await getLocationId();
